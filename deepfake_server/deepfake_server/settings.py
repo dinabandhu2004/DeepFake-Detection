8c43f2b851d6305f9af67101b9673b5a5af59d0f
Dinabandhu Barai <dinabandhu2004barai@gmail.com>
2025-11-18 23:33:03 +0530

    locathost allowed

--- deepfake_server/deepfake_server/settings.py
+++ deepfake_server/deepfake_server/settings.py
@@ -30,6 +30,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'deepfake_server.urls'
